39ec92a7b2e82cfcabb1ddde076c1c5ec21b9a02
Alejandro Toledo <jairoalejandro2003@gmail.com>
2025-06-08 13:38:57 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProduction = process.env.ENVIROMENT === 'PROD'
+const repoName = process.env.REPO_NAME || '';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: isProduction ? `/${repoName}/` : '/',
 })
