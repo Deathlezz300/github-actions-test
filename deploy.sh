c19480e63c3db2c57ca5dc1f9c1c9291b59105c6
Alejandro Toledo <jairoalejandro2003@gmail.com>
2025-06-21 13:55:26 -0500

    si

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ ssh -o StrictHostKeyChecking=no azureuser@172.174.226.182 <<HTML
     cd "/home/azureuser/github-actions-test"
 
     git pull origin azure-deploy-vm-test
+    rm -rf node_modules package-lock.json
     npm install 
     npm run build
 
